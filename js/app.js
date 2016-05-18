fc798298a50267a0e889745da8a985439f52f614
En Sabah Nur <kelsey.jackson@rockfishinteractive.com>
2016-05-17 20:53:28 -0500

    added custom icons and fooled around with the fullscreen toggle

--- js/app.js
+++ js/app.js
@@ -1,16 +1,16 @@
 var videoPlayer = {
     vid: document.getElementById('video-id'),
-    playBtn: document.querySelector('.inner-controls-1').children[0],
-    soundBtn: document.querySelector('.inner-controls-2').children[0],
-    fullScreen: document.querySelector('.inner-controls-2').children[1],
+    playBtn: document.getElementById('play'),
+    soundBtn: document.getElementById('volume'),
+    fullScreenBtn: document.getElementById('fullscreen'),
     playBack: function () {
         if(videoPlayer.vid.paused) {
             videoPlayer.vid.play();
-            videoPlayer.playBtn.src = 'icons/pause-icon.png';
+            videoPlayer.playBtn.src = 'icons/pause.svg';
             console.log('playing');
         } else {
             videoPlayer.vid.pause();
-            videoPlayer.playBtn.src = 'icons/play-icon.png';
+            videoPlayer.playBtn.src = 'icons/play_arrow.svg';
             console.log('paused');
         }
 
@@ -18,22 +18,39 @@ var videoPlayer = {
     sound: function () {
         if(videoPlayer.vid.muted) {
             videoPlayer.vid.muted = false;
+            videoPlayer.soundBtn.src = 'icons/volume_up.svg';
             console.log('sound on');
         } else {
             videoPlayer.vid.muted = true;
+            videoPlayer.soundBtn.src = 'icons/volume_off.svg';
             console.log('sound off');
         }
     },
-    goFullscreen: function () {
-        if(videoPlayer.vid.requestFullscreen) {
-            videoPlayer.vid.requestFullscreen();
-        } else if(videoPlayer.vid.mozrequestFullscreen) {
-            videoPlayer.vid.mozrequestFullscreen();
-        } else if(videoPlayer.vid.webkitRequestFullscreen) {
-            videoPlayer.vid.webkitRequestFullscreen();
-        } else if(videoPlayer.vid.msRequestFullscreen) {
-            videoPlayer.vid.msRequestFullscreen();
+    toggleFullscreen: function () {
+        if(!document.fullscreenElement && !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement) {
+            if(videoPlayer.vid.requestFullscreen) {
+                videoPlayer.vid.requestFullscreen();
+            } else if(videoPlayer.vid.mozrequestFullscreen) {
+                videoPlayer.vid.mozrequestFullscreen();
+            } else if(videoPlayer.vid.webkitRequestFullscreen) {
+                videoPlayer.vid.webkitRequestFullscreen();
+            } else if(videoPlayer.vid.msRequestFullscreen) {
+                videoPlayer.vid.msRequestFullscreen();
+            }
+            videoPlayer.fullScreenBtn.src = 'icons/fullscreen_exit.svg';
+        } else {
+            if (document.exitFullscreen) {
+                document.exitFullscreen();
+            } else if (document.mozCancelFullScreen) {
+                document.mozCancelFullScreen();
+            } else if (document.webkitExitFullscreen) {
+                document.webkitExitFullscreen();
+            } else if (document.msExitFullscreen) {
+                document.msExitFullscreen();
+            }
+            videoPlayer.fullScreenBtn.src = 'icons/fullscreen.svg';
         }
+
     }
 };
 
@@ -41,4 +58,12 @@ var videoPlayer = {
 
 videoPlayer.playBtn.addEventListener('click', videoPlayer.playBack);
 videoPlayer.soundBtn.addEventListener('click', videoPlayer.sound);
-videoPlayer.fullScreen.addEventListener('click', videoPlayer.goFullscreen);
+videoPlayer.fullScreenBtn.addEventListener('click', videoPlayer.toggleFullscreen);
+
+/*
+
+ videoPlayer.vid.textTracks[0].mode
+ "showing"
+ videoPlayer.vid.textTracks[0].mode = 'hidden'
+ "hidden"
+ */
